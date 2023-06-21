2d989f17aa4101b2db8aebbaed562b2caf797cbe
vedant-3010 <vedantdandgepro@gmail.com>
2023-06-21 23:39:55 +0530

    minor changes

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,11 +1,7 @@
 const asyncHandler = asyncHandler("express-async-handler");
 
 const accessChat = asyncHandler(async (req, res) => {
-    const {userId} = req.body;
-
-    if(!userId){
-        res.status(400);
-        throw new Error("Invalid user ID");
-    }
+    
+    
 
 });
